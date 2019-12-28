955145d8e2cc4fa74c4edf719b93b05cddecca8f
darkhawk0000 <liur3@tcnj.edu>
2019-12-28 15:57:55 -0500

    Inital commit

---
+++ cloudenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/darkhawk0000/ComputerScience/FilePartition/filesecurity/cloudenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ cloudsecurity/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class FileDetails(models.Model):
+    owner = models.CharField(max_length=200)
+    file_name = models.CharField(max_length=200)
+    file_type = models.CharField(max_length=7)
+    head_id = models.IntegerField()
+    head_table = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __str__(self):
+        return self.file_name
+
+class FilePartitionOne(models.Model):
+    byte_array = models.BinaryField()
+    next_id = models.IntegerField()
+    next_table = models.IntegerField()
+    def __str__(self):
+        return self.id
+
+class FilePartitionTwo(models.Model):
+    byte_array = models.BinaryField()
+    next_id = models.IntegerField()
+    next_table = models.IntegerField()
+    def __str__(self):
+        return self.id
+
+class FilePartitionThree(models.Model):
+    byte_array = models.BinaryField()
+    next_id = models.IntegerField()
+    next_table = models.IntegerField()
+    def __str__(self):
+        return self.id
+
+class FilePartitionFour(models.Model):
+    byte_array = models.BinaryField()
+    next_id = models.IntegerField()
+    next_table = models.IntegerField()
+    def __str__(self):
+        return self.id
